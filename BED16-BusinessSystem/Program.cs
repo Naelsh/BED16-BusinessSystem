078799a8e778b5d186cc581c5e462c5fcfc89b66
Niklas Lindblad <niklas.o.lindblad@gmail.com>
2016-12-27 15:15:53 +0100

    Initial Setup
    Added base C# project

---
+++ BED16-BusinessSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BED16_BusinessSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
